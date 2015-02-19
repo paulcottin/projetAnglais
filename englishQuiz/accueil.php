4ba3d6cfcddd1038293c5d8a1aeb93d2292e681e
Paul Cottin <paulcottin@gmail.com>
2015-02-19 17:43:47 +0100

    ajout de la page d'accueil et du fichier CSS

---
+++ englishQuiz/accueil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>English Quiz</title>
+    </head>
+    <body>
+    <p class="centerWhite70">English Quiz</p>
+        <div class="align">
+            <input type="button" class="button" name="choix" value="Jouer"/><br/>
+            <input type="button" class="button" name="choix" value="Classement"/><br/>
+            <input type="button" class="button" name="choix" value="Proposer une question"/><br/>
+        </div>
+    </body>
+</html>
