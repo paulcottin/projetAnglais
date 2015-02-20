3d3873411be41058ac59735ce48a7d4d1ec61000
Jean-Laurent <jean-laurent.brison@telecomnancy.net>
2015-02-20 18:50:11 +0100

    Resultats

--- englishQuiz/ajoutQuestion.php
+++ englishQuiz/ajoutQuestion.php
@@ -19,24 +19,25 @@
                        <label for="theme" class="text20">Thema ?</label><br />
                        <select name="theme" id="theme">
                             <option value="default" selected>--</option>
-                            <option value="History">History</option>
                             <option value="Geography">Geography</option>
-                            <option value="Spelling">Spelling</option>
-                            <option value="Grammary">Grammary</option>
+                            <option value="Grammar">Grammar</option>
+                            <option value="History">History</option>
                             <option value="Literature">Literature</option>
-                            <option value="Politics and busines">Politics and business</option>
+                            <option value="Politics and business">Politics and business</option>
                             <option value="Series/Cinema/Music">Series/Cinema/Music</option>
+                            <option value="Spelling">Spelling</option>  
+                            <option value="Verbs">Verbs</option>
                             <option value="Vocabulary">Vocabulary</option>
-                       </select>
+                        </select>
                 </span>
                 <span style="float:right;vertical-alignement:top">
-                        <p class="text20">Responses :</p>
-                        <input type="text" class="questionButton" name="rep1" placeholder="true response"/>
-                        <input type="text" class="questionButton" name="rep2" placeholder="false response"/>
+                        <p class="text20">answers :</p>
+                        <input type="text" class="questionButton" name="rep1" placeholder="true answer"/>
+                        <input type="text" class="questionButton" name="rep2" placeholder="false answer"/>
                         <br/>
                         <div>
-                            <input type="text" class="questionButton" name="rep3" placeholder="false response"/>
-                            <input type="text" class="questionButton" name="rep4" placeholder="false response"/>
+                            <input type="text" class="questionButton" name="rep3" placeholder="false answer"/>
+                            <input type="text" class="questionButton" name="rep4" placeholder="false answer"/>
                         </div>
                 </span>
             </form>
--- englishQuiz/resultat.php
+++ englishQuiz/resultat.php
@@ -1,8 +1,56 @@
-<?php
-session_start();
-session_unset($_SESSION['nb_qts_posees']);
+<!DOCTYPE html>
+<?php session_start();
+/*session_unset($_SESSION['nb_qts_posees']);
 session_unset($_SESSION['qts_posees']);
 
  echo "votre score est magnifique";
-
+*/
+$_SESSION['score'] = 0
 ?>
+
+<html>
+<head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>English Quiz</title>
+    </head>
+    <body>
+    <p class="centerWhite70">English Quiz</p>
+	<head>
+    	<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css">
+			    <p class="centerWhite50"><?php echo $_SESSION['score']; ?>/20</p>
+			    <?php 	if ($_SESSION['score'] <= 4){
+			    			?> 
+			    			<p class="centerWhite50">Not very good...</p>
+			    			<?php 
+						}
+						if ($_SESSION['score'] > 4 && $_SESSION['score'] <= 8){
+							?> 
+					    	<p class="centerWhite50">You still have to progress</p>
+							<?php 
+						}
+						if ($_SESSION['score'] > 8 && $_SESSION['score'] <= 12){
+							?> 
+					    	<p class="centerWhite50">Not so bad</p>
+					    	<?php 
+						}
+						if ($_SESSION['score'] > 12 && $_SESSION['score'] <= 16){
+					    	?> 
+					    	<p class="centerWhite50">Good !</p>
+					    	<?php 
+						}
+						if ($_SESSION['score'] > 16 && $_SESSION['score'] < 20){
+					    	?> 
+					    	<p class="centerWhite50">Excellent !!!</p>
+					    	<?php 
+						}
+						?>
+
+
+    
+        <div class="align">
+            <input type="button" class="button" name="home" value="Home"/><br/>
+        </div>
+    </body>
+</html>
